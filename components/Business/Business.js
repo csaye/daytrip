fcf276a024b190d47a7afd4911b5037f129d2b15
Cooper Saye <coopersaye@gmail.com>
2021-06-27 00:03:59 -0700

    Improve Business styling.

--- components/Business/Business.js
+++ components/Business/Business.js
@@ -2,24 +2,26 @@ import styles from './Business.module.css';
 
 export default function Business(props) {
   const { image_url, name, location, rating, review_count, categories } = props.business;
-  const { address1, address2, address3, city, state, zip_code } = location;
+  const { address1, city, state, zip_code } = location;
 
   return (
-    <div>
-      <img src={image_url} alt=""/>
-      <h2>{name}</h2>
-      <div>
-        <div>
-          <p>{address1}</p>
-          {address2 && <p>{address2}</p>}
-          {address3 && <p>{address3}</p>}
-          <p>{city}</p>
-          <p>{state} {zip_code}</p>
-        </div>
-        <div>
-          <h3>{categories.map(category => category.title).join(', ')}</h3>
-          <h3>{rating} stars</h3>
-          <p>{review_count} reviews</p>
+    <div className={styles.container}>
+      <div className={styles.cover}>
+        <img src={image_url} alt=""/>
+      </div>
+      <div className={styles.summary}>
+        <h2 className={styles.title}>{name}</h2>
+        <div className={styles.info}>
+          <div>
+            <p>{address1}</p>
+            <p>{city}</p>
+            <p>{state} {zip_code}</p>
+          </div>
+          <div className={styles.rating}>
+            <p>{categories[0].title}</p>
+            <p>{rating} stars</p>
+            <p>{review_count} reviews</p>
+          </div>
         </div>
       </div>
     </div>
