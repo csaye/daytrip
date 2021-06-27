e47e39096748e2c96d867bc3ca64e87247072eb0
Cooper Saye <coopersaye@gmail.com>
2021-06-27 08:55:24 -0700

    Fix fullcalendar css imports.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
-module.exports = {
+// for transpiling all ESM @fullcalendar/* packages
+// also, for piping fullcalendar thru babel (to learn why, see babel.config.js)
+
+const withTM = require('next-transpile-modules')([
+  // Need to specify all @fullcalendar modules separately
+  // with next-transpile-modules^6.x …
+  '@fullcalendar/common',
+  '@fullcalendar/daygrid',
+  '@fullcalendar/interaction',
+  '@fullcalendar/react',
+  '@fullcalendar/timegrid'
+])
+
+module.exports = withTM({
   reactStrictMode: true,
-}
+})
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
 import '../styles/globals.css';
+import '@fullcalendar/common/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
 
 export default function App({ Component, pageProps }) {
   return (
