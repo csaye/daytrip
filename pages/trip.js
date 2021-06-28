487c8ddadf3b2c6f577739d222f494081df67918
Cooper Saye <coopersaye@gmail.com>
2021-06-27 23:46:03 -0700

    Redo Yelp search and business list.

--- pages/trip.js
+++ pages/trip.js
@@ -1,10 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import Business from '../components/Business/Business.js';
 import Link from 'next/link';
 
 import { useRouter } from 'next/router';
 
+import styles from '../styles/Trip.module.css';
+
 export default function Trip() {
+  const [businesses, setBusinesses] = useState(undefined);
+
   const router = useRouter();
+
+  // searches yelp api for matching parameters
+  async function search(queryObj) {
+    // get query string
+    const query = Object.keys(queryObj)
+    .map(key => `${key}=${queryObj[key]}`).join('&');
+    // get api response
+    const response = await fetch(`/api/yelp?${query}`);
+    const json = await response.json();
+    return response.ok ? json.businesses : undefined;
+  }
 
   useEffect(() => {
     if (router.isReady) {
@@ -15,6 +31,17 @@ export default function Trip() {
   return (
     <div>
       <Link href="/">Home</Link>
+      {
+        businesses ?
+        <div className={styles.businesslist}>
+          {
+            businesses.map(business =>
+              <Business key={business.id} business={business} />
+            )
+          }
+        </div> :
+        <p>Loading businesses...</p>
+      }
     </div>
   );
 }
