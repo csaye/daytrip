bfbcfc2ba92fcf8e1452ceedc3c379837f229d10
Cooper Saye <coopersaye@gmail.com>
2021-06-28 12:20:52 -0700

    Add loading text.

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
   const [location, setLocation] = useState('');
   const [events, setEvents] = useState([]);
 
@@ -21,7 +22,9 @@ export default function Home() {
     router.push(`/trip?${query}`);
   }
 
+  // searches with current location
   function searchCurrentLocation() {
+    setLoading(true);
     navigator.geolocation.getCurrentPosition(
       pos => {
         // search with latitude and longitude
@@ -29,7 +32,10 @@ export default function Home() {
         const longitude = pos.coords.longitude;
         search({ latitude, longitude });
       },
-      error => console.warn(error)
+      error => {
+        console.warn(error);
+        setLoading(false);
+      }
     );
   }
 
@@ -38,24 +44,31 @@ export default function Home() {
       <div className={styles.header}>
         <h1>Daytrip</h1>
       </div>
-      <div className={styles.searchbar}>
-        <button onClick={searchCurrentLocation}>
-          Search with Current Location
-        </button>
-        <form onSubmit={e => {
-          e.preventDefault();
-          // search with manual location
-          search({ location });
-        }}>
-          <input
-            value={location}
-            onChange={e => setLocation(e.target.value)}
-            required
-          />
-          <button>Search with Manual Location</button>
-        </form>
-      </div>
-      <Calendar setEvents={setEvents} />
+      {
+        loading ?
+        <p>Loading...</p> :
+        <>
+          <div className={styles.searchbar}>
+            <button onClick={searchCurrentLocation}>
+              Search with Current Location
+            </button>
+            <form onSubmit={e => {
+              e.preventDefault();
+              // search with manual location
+              setLoading(true);
+              search({ location });
+            }}>
+              <input
+                value={location}
+                onChange={e => setLocation(e.target.value)}
+                required
+              />
+              <button>Search with Manual Location</button>
+            </form>
+          </div>
+          <Calendar setEvents={setEvents} />
+        </>
+      }
     </div>
   );
 }
