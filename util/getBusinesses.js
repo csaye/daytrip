960493cbc9989182b89022ad885793913ceb1052
Cooper Saye <coopersaye@gmail.com>
2021-06-28 23:29:35 -0700

    Extract getBusinesses function.

---
+++ util/getBusinesses.js
@@ -0,0 +1,32 @@
+// searches yelp api for matching parameters
+async function yelpSearch(queryObj) {
+  // get query string
+  const query = Object.keys(queryObj)
+  .map(key => `${key}=${queryObj[key]}`).join('&');
+  // get api response
+  const response = await fetch(`/api/yelp?${query}`);
+  const json = await response.json();
+  return response.ok ? json.businesses : undefined;
+}
+
+// gets businesses based on given search query and events
+export default async function getBusinesses(query, events) {
+  const businesses = [];
+  // for each event
+  for (const e of events) {
+    // yelp search with query and term
+    const result = await yelpSearch({ ...query, term: e.term });
+    // continue if invalid result
+    if (!result?.length) continue;
+    // append random business from result
+    const randomIndex = Math.floor(Math.random() * result.length);
+    const business = {
+      ...result[randomIndex],
+      term: e.term,
+      timespan: { start: e.start, end: e.end }
+    };
+    businesses.push(business);
+  }
+  // return businesses
+  return businesses;
+}
