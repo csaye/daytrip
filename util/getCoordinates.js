74f9a61bc38f2c1a488bbd1a8a8447c3927257c6
Cooper Saye <coopersaye@gmail.com>
2021-07-03 17:38:15 -0700

    Remove geocoding route.

--- util/getCoordinates.js
+++
@@ -1,6 +0,0 @@
-export default async function getCoordinates(address) {
-  // make request
-  const response = await fetch(`/api/coordinates?address=${address}`);
-  const json = await response.json();
-  return response.ok ? json : undefined;
-}
